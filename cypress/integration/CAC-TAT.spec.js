e2855385712c086e61df212c63a80ffdb8383e05
Lucas Cardoso Ferreira <elucasferreira@gmail.com>
2024-12-26 14:37:12 -0300

    Finalizando seção 7

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -118,4 +118,28 @@ it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é p
     cy.get('button[type="submit"]').click()
     cy.get('.error').should('be.visible')
  })
+ it('seleciona um arquivo da pasta fixtures', function(){
+    cy.get('input[type="file"]')
+    .should('not.have.value')
+    .selectFile('cypress/fixtures/example.json')
+    .should(function($input){
+        expect($input[0].files[0].name).to.equal('example.json')
+    })
+ })
+ it('seleciona um arquivo simulando um drag-and-drop', function(){
+    cy.get('input[type="file"]')
+    .should('not.have.value')
+    .selectFile('cypress/fixtures/example.json', { action: 'drag-drop'})
+    .should(function($input){
+        expect($input[0].files[0].name).to.equal('example.json')
+    })
+ })
+ it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('input[type="file"]')
+    .selectFile('@sampleFile')
+    .should(function($input){
+        expect($input[0].files[0].name).to.equal('example.json')
+    })
+ })
 })
